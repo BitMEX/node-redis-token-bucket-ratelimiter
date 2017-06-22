d959837ce3dc37a7cd38847cbd67616b72bf157e
Jason Bond Pratt <jason@bitmex.com>
2017-06-22 10:22:50 -0700

    Removed unused logging, cleaned up whitespace, added stubLimit

--- tests/test.js
+++ tests/test.js
@@ -21,12 +21,7 @@ exports.use = function(test) {
     let numLeft = 3;
     (function useNext() {
         // 3/5000
-        defaultLimiter.use('use1', function(err, res) {
-            if (err) {
-                console.log('use1 error', err);
-                test.done();
-                return;
-            }
+      defaultLimiter.use('use1').then(function(res) {
             if (numLeft) {
                 test.equal(res.remaining, numLeft - 1);
                 test.equal(res.rejected, false);
@@ -40,55 +35,54 @@ exports.use = function(test) {
               test.equal(res.rejected, true);
               test.done();
             }
-        });
+      })
+      .catch(function(err){
+        console.log('use1 error', err);
+        test.done();
+      });
     }());
 };
 
 exports.expires = function(test) {
-    test.expect(2);
-    var limiter = new RollingLimit({
-        interval: 250,
-        limit: 3,
-        redis: redisClient,
-        prefix: prefix
+  test.expect(2);
+  var limiter = new RollingLimit({
+    interval: 250,
+    limit: 3,
+    redis: redisClient,
+    prefix: prefix
+  });
+  
+  limiter.use('ttl').then(function(res) {
+    redisClient.pttl(prefix + 'limit:ttl', function(err, res) {
+      if (err) {
+        console.log('prefix ttl error', err);
+        test.done();
+        return;
+      }
+
+      test.ok(50 < res && res <= 250);
     });
-
-    limiter.use('ttl', function(err,res) {
-        if (err) {
-            console.log('ttl error', err);
-            test.done();
-            return;
-        }
-
-        redisClient.pttl(prefix + 'ttl', function(err, res) {
-            if (err) {
-                console.log('prefix ttl error', err);
-                test.done();
-                return;
-            }
-
+    
+    setTimeout(function() {
+      limiter.use('ttl').then(function(res) {
+        redisClient.pttl(prefix + 'limit:ttl', function(err, res) {
+          if (err) console.log('prefix ttl error', err);
+          else {
             test.ok(50 < res && res <= 250);
-        });
-      
-        setTimeout(function() {
-            limiter.use('ttl', function(err, res) {
-                if (err) {
-                    console.log('ttl error', err);
-                    test.done();
-                    return;
-                }
-
-                redisClient.pttl(prefix + 'ttl', function(err, res) {
-                    if (err) console.log('prefix ttl error', err);
-                    else {
-                        test.ok(50 < res && res <= 250);
-                        test.done();
-                    }
-                });
-      
-            });
-        }, 500);
-    });
+            test.done();
+          }
+        });        
+      })
+      .catch(function(err){
+        console.log('ttl error', err);
+        test.done();
+      });
+    }, 500);
+  })
+  .catch(function(err){
+    console.log('ttl error', err);
+    test.done();
+  });
 };
 
 exports.rolling = function(test) {
@@ -101,51 +95,48 @@ exports.rolling = function(test) {
     });
 
     setTimeout(function() {
-      limiter.use('rolling100', 2, function(err, res) {
-          if (err) {
-              console.log('rolling100 error', err);
-              test.done();
-              return;
-          }
-          test.equal(res.rejected, false);
-          test.equal(res.remaining, 0);
-          test.ok(400 < res.retryDelta && res.retryDelta <= 500);
+      limiter.use('rolling100', 2).then(function(res) {
+        test.equal(res.rejected, false);
+        test.equal(res.remaining, 0);
+        test.ok(400 < res.retryDelta && res.retryDelta <= 500);
+      })
+      .catch(function(err){
+        console.log('rolling100 error', err);
+        test.done();
       });
     },0);
 
     setTimeout(function() {
-        limiter.use('rolling100', 1, function(err, res) {
-            if (err) {
-                console.log('rolling100 in setTimeout1 error', err);
-                return;
-            }
-            test.equal(res.rejected, true);
-            test.ok(0 < res.retryDelta && res.retryDelta <= 100);
-        });
+      limiter.use('rolling100', 1).then(function(res) {
+        test.equal(res.rejected, true);
+        test.ok(0 < res.retryDelta && res.retryDelta <= 100);
+      })
+      .catch(function(err){
+        console.log('rolling100 in setTimeout1 error', err);
+        return;
+      });
     }, 150);
 
     setTimeout(function() {
-        limiter.use('rolling100', 1, function(err, res) {
-            if (err) {
-                console.log('rolling100 in setTimeout1 error', err);
-                return;
-            }
-            test.equal(res.rejected, false);
-            test.equal(res.remaining, 0);
-            test.ok(150 < res.retryDelta && res.retryDelta <= 250);
-        });
+      limiter.use('rolling100', 1).then(function(res) {
+        test.equal(res.rejected, false);
+        test.equal(res.remaining, 0);
+        test.ok(150 < res.retryDelta && res.retryDelta <= 250);
+      })
+      .catch(function(err){
+        console.log('rolling100 in setTimeout1 error', err);
+      });
     }, 250);
 
     setTimeout(function() {
-        limiter.use('rolling100', 1, function(err, res) {
-            if (err) {
-                console.log('rolling100 in setTimeout1 error', err);
-                return;
-            }
-            test.equal(res.remaining, 1);
-            test.equal(res.rejected, false);
-            test.done();
-        });
+      limiter.use('rolling100', 1).then(function(res) {
+        test.equal(res.remaining, 1);
+        test.equal(res.rejected, false);
+        test.done();
+      })
+      .catch(function(err){
+        console.log('rolling100 in setTimeout1 error', err);
+      });
     }, 850);
 
 };
@@ -165,13 +156,12 @@ exports.useMultiple = function(test) {
 
 exports.useMoreThanLimit = function(test) {
     test.expect(1);
-    try{
-        defaultLimiter.use('use4', 4);
-    }
-    catch(err){
-        test.ok(err !== undefined);
-    }  
-    test.done();
+  defaultLimiter.use('use4', 4)
+    .then(test.done)
+    .catch(function(){
+      test.ok(true);
+      test.done();
+    })
 };
 
 exports.useZero = function(test) {
