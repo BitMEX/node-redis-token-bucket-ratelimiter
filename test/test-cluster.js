e29bf274a4e9e4066f77a7a2355093477de979c3
Samuel Reed <samuel.trace.reed@gmail.com>
2018-02-07 13:32:59 -0600

    test(cluster): addl cluster test

---
+++ test/test-cluster.js
@@ -0,0 +1,22 @@
+'use strict';
+const Redis = require('ioredis');
+const Promise = require('bluebird');
+const {makeTestSuite} = require('./_test-common');
+const port = Number(process.env.PORT) || 7001;
+const host = process.env.HOST || '127.0.0.1';
+
+const redisClient = new Redis.Cluster([
+  {port,host},
+  {port: port + 1, host},
+  {port: port + 2, host},
+], {
+  scaleReads: 'all',
+});
+Promise.promisifyAll(redisClient);
+
+redisClient.on('error', (e) => {
+  console.error(`Error connecting to Redis Cluster. Did you start a local server on ${port}?`);
+  console.error(e);
+});
+
+makeTestSuite('cluster client', redisClient, 300 /* lag */);
