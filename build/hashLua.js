126f134ce1c10d4369b6a6f6393537d03a8a6fe0
Samuel Reed <samuel.trace.reed@gmail.com>
2018-02-07 11:22:05 -0600

    refactor(hash): simplify build

--- build/hashLua.js
+++ build/hashLua.js
@@ -1,19 +1,11 @@
-var fs = require('fs'),
+const fs = require('fs'),
     sha1 = require('sha1'),
     filename = process.argv[2];
 
-fs.readFile(filename, {encoding: 'utf8'}, function(err, contents) {
-    if (err) {
-        throw err;
-    }
-    var result = {
-        script: contents,
-        sha1: sha1(contents)
-    };
-    fs.writeFile(filename + '.json', JSON.stringify(result), function(err) {
-        if (err) {
-            throw err;
-        }
-        process.exit(0);
-    });
-});
+const contents = fs.readFileSync(filename, {encoding: 'utf8'});
+const result = {
+    script: contents,
+    sha1: sha1(contents)
+};
+fs.writeFileSync(filename + '.json', JSON.stringify(result));
+process.exit(0);
